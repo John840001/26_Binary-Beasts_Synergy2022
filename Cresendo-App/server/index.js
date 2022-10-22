cd252304a48bf58adf73af4409538e473e904054
Jonathan Duarte <jonathanduarte840@gmail.com>
2022-10-22 09:25:59 +0530

    Started Cresendo App

---
+++ Cresendo-App/server/index.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const connection = require("./db");
+const userRoutes = require("./routes/users");
+const authRoutes = require("./routes/auth");
+// const payment = require("./routes/payment");
+
+// database connection
+connection();
+
+// middlewares
+app.use(express.json());
+app.use(cors());
+
+// routes
+app.use("/api/users", userRoutes);
+app.use("/api/auth", authRoutes);
+// app.use("/api/payment", payment);
+
+const port = process.env.PORT || 5001;
+app.listen(port, console.log(`Listening on port ${port}...`));
